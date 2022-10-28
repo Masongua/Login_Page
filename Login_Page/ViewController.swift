6ee1877d577c419b5f0776abec6526b9f42af8ba
Mason Guaman <masonstalin@gmail.com>
2022-10-28 13:04:33 -0400

    Initial Commit

---
+++ Login_Page/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Login_Page
+//
+//  Created by 5e on 10/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
