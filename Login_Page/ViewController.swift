75300eb05b635893ea86440a5c49e3cf07e5c00c
Mason Guaman <masonstalin@gmail.com>
2022-10-28 14:03:37 -0400

    ty

--- Login_Page/ViewController.swift
+++ Login_Page/ViewController.swift
@@ -14,6 +14,39 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var userName: UITextField!
+    
+    @IBOutlet weak var forgotPassword: UIButton!
+    
+    @IBOutlet weak var forgotUsername: UIButton!
+    
+    
+    @IBAction func passwordAct(_ sender: Any) {
+        performSegue(withIdentifier: "userNameAndPassword", sender: sender)
+        
+    }
+    
+    
+    @IBAction func usernameAct(_ sender: Any) {
+        performSegue(withIdentifier: "userNameAndPassword", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = userName.text
+        guard let sender = sender as? UIButton else {
+            return
+        }
+        if sender == forgotPassword {
+            segue.destination.navigationItem.title = "Forgot Password"
+        } else if sender == forgotUsername {
+            segue.destination.navigationItem.title = "Forgot Username"
+        } else {
+            segue.destination.navigationItem.title = userName.text
+        }
+    }
+    
+    
+    
+    
 }
 
